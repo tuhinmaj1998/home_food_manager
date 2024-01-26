6269e273bde7daabb555dcdaafe8b6ebf06a5582
tuhinmajumder <tuhinmaj1998@gmail.com>
2024-01-27 01:27:22 +0530

    first_commit

---
+++ db_connect.py
@@ -0,0 +1,86 @@
+import psycopg2
+import pandas as pd
+
+
+host = 'rain.db.elephantsql.com'
+database = 'rqlhbipg'
+username = 'rqlhbipg'
+password = 'nFReJlmf7g33cBJB0zgug5Q8S1m_aOAX'
+port = 5432
+
+
+def db_cursor():
+    conn = psycopg2.connect(host=host, dbname=database, user=username, password=password, port=port)
+    return conn
+
+
+def db_collect_staples():
+    conn = db_cursor()
+    cursor = conn.cursor()
+    cursor.execute(f"""SELECT staple_name FROM "home_food_manager"."staples";""")
+    staples = [i[0] for i in cursor.fetchall()]
+    return staples
+
+
+def db_collect_dishes(staple_id):
+    conn = db_cursor()
+    cursor = conn.cursor()
+    cursor.execute(f"""
+    select dish_name from home_food_manager.dish_suggestions ds
+    join home_food_manager.staples s on s.staple_id = ds.staple_id
+    join home_food_manager.dishes d on d.dish_id = ds.dish_id where ds.staple_id = {staple_id}
+    group by dish_name;
+    """)
+    dishes = [i[0] for i in cursor.fetchall()]
+    return dishes
+
+
+# print(db_collect_dishes())
+
+def db_add_daily_menu(staple, dish1, dish2, remarks=None):
+    conn = db_cursor()
+    cursor = conn.cursor()
+    cursor.execute(f"""
+        select daily_menu_id from  home_food_manager.daily_menu 
+        where substring(cast(createddate+interval '5.5 hour' as varchar), 1,10) = substring(cast(now()+interval '5.5 hour' as varchar), 1,10)
+        limit 1;""")
+    daily_menu_id = None
+    if_exist_id = cursor.fetchone()
+
+    if if_exist_id is not None:
+
+        will_be_updated = 1
+
+        daily_menu_id = if_exist_id[0]
+        # print(daily_menu_id)
+    else:
+        will_be_updated = 0
+
+    if will_be_updated:
+        print("update")
+        q = f"""
+        update home_food_manager.daily_menu 
+        set staple = '{staple}', dish1 = '{dish1}', dish2 = '{dish2}' 
+        where daily_menu_id = {daily_menu_id};"""
+        print(q)
+        cursor.execute(q)
+        conn.commit()
+
+    else:
+        cursor.execute(f"""
+        insert into home_food_manager.daily_menu("staple", "dish1", "dish2", "createddate")
+        values('{staple}', '{dish1}', '{dish2}', now()+interval '5.5 hour');""")
+        conn.commit()
+
+def db_get_all_daily_menu():
+    conn = db_cursor()
+    cursor = conn.cursor()
+    q = f"""
+            select substring(cast(createddate+interval '5.5 hour' as varchar), 1,10) as createddate, staple, dish1, dish2, remarks  from  home_food_manager.daily_menu 
+            order by createddate desc;"""
+    # cursor.execute()
+    # daily_menus = [i for i in cursor.fetchall()]
+    daily_menu = pd.read_sql_query(q, conn)
+    return daily_menu
+
+# print(db_get_all_daily_menu())
---
+++ main.py
@@ -0,0 +1,99 @@
+import json
+
+from fastapi import FastAPI, Request
+# from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+
+import db_connect
+
+app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+# class model1(BaseModel):
+#     mytext:str
+
+result = []
+
+
+#
+# @app.post("/submit", response_class=HTMLResponse)
+# @app.get("/submit", response_class=HTMLResponse)
+# async def submit_form(request: Request):
+#     if request.method == 'GET':
+#         input1 = int(request.query_params.get("input1", 0))
+#         return templates.TemplateResponse("home3.html", {"request": request, "input1": input1})
+#     elif request.method == 'POST':
+#         form_data = await request.form()
+#         input1 = int(form_data.get("input1", 0))
+#         print(input1)
+#         db_result = db_connect.db_test(input1)
+#         result.append(f'{db_result[1]}:{db_result[2]}')
+#         return templates.TemplateResponse("home3.html", {"request": request, "input1": input1, "result": result})
+
+# @app.post("/choose", response_class=HTMLResponse)
+@app.get("/choose", response_class=HTMLResponse)
+async def choose(request: Request):
+    if request.method == 'GET':
+        input1 = int(request.query_params.get("input1", 0))
+        return templates.TemplateResponse("choose.html", {"request": request, "input1": input1})
+
+
+@app.post("/submit")
+async def submit(request: Request):
+    form_data = await request.json()
+    staple = form_data.get("staple")
+    dish1 = form_data.get("dish1")
+    dish2 = form_data.get("dish2")
+
+    # Use the received data as needed
+    result = {
+        "staple": staple,
+        "dish1": dish1,
+        "dish2": dish2,
+    }
+    print(result)
+    db_connect.db_add_daily_menu(result['staple'], result['dish1'], result['dish2'])
+    # return RedirectResponse(url='/all_result')
+
+
+@app.get("/all_results")
+async def all_results(request: Request):
+    df = db_connect.db_get_all_daily_menu()
+
+    # Convert DataFrame to JSON directly
+    json_data = df.to_json(orient='records')
+    print(json_data)
+
+    return templates.TemplateResponse("daily_menu.html",
+                                      {
+                                          "request": request,
+                                          "data": json_data
+                                      })
+
+
+@app.get("/api/staples")
+async def get_staples():
+    staples = db_connect.db_collect_staples()
+    return JSONResponse(content={"staples": staples})
+
+
+@app.get("/api/dishes/{staple_id}")
+async def get_dishes(staple_id):
+    dishes = db_connect.db_collect_dishes(staple_id)
+    # print(dishes)
+    return JSONResponse(content={"dishes": dishes})
+
+#
+# @app.get("/api/staples")
+# async def get_staples():
+#     staples = db_connect.db_collect_staples()
+#     return JSONResponse(content={"staples": staples})
